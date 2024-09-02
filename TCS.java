3559563152434bc72a5e1d39e162757224f9dbd0
Atupe29 <atupe439@gmail.com>
2024-09-02 12:46:30 +0530

    file uplode secussfully

---
+++ TCS.java
@@ -0,0 +1,16 @@
+Started by timer
+Running as SYSTEM
+Building in workspace C:\ProgramData\Jenkins\.jenkins\workspace\WIPRO
+[WIPRO] $ cmd /c call C:\WINDOWS\TEMP\jenkins2699326222770419867.bat
+
+C:\ProgramData\Jenkins\.jenkins\workspace\WIPRO>javac Arjun.java 
+error: file not found: Arjun.java
+Usage: javac <options> <source files>
+use --help for a list of possible options
+
+C:\ProgramData\Jenkins\.jenkins\workspace\WIPRO>java Arjun 
+Error: Could not find or load main class Arjun
+Caused by: java.lang.ClassNotFoundException: Arjun
+
+C:\ProgramData\Jenkins\.jenkins\workspace\WIPRO>exit 1 
+Build step 'Execute W
